cfc90ae3c846378cd60a266897b7163eca6d92cf
Jeet <akarshan.biswas@gmail.com>
2017-11-23 21:02:56 +0530

    Update uninstall_run_in_terminal.sh

--- uninstall_run_in_terminal.sh
+++ uninstall_run_in_terminal.sh
@@ -3,15 +3,14 @@
 
 
 
-sudo rm -v power.sh /etc/rc.d/power.sh
+sudo rm -v /etc/rc.d/power.sh
 
-sudo rm -v power.service /etc/systemd/system/power.service
-sudo rm -v powersave.rules /etc/udev/rules.d/powersave.rules
-sudo rm -v root-resume.service /etc/systemd/system/root-resume.service
+sudo rm -v  /etc/systemd/system/power.service
+sudo rm -v  /etc/udev/rules.d/powersave.rules
+sudo rm -v  /etc/systemd/system/root-resume.service
 sudo systemctl daemon-reload
 sudo systemctl disable power.service
 sudo systemctl disable root-resume.service
-sudo systemctl stop power.service
 sudo udevadm control --reload-rules
 
 echo "UNinstallation complete."
